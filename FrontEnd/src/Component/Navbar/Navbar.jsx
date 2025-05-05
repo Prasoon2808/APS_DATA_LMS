f38233361ffa856a03b2dea85af0f6233b37ca28
guptadhruv1901 <guptadhruv1901@gmail.com>
2025-05-05 23:29:45 +0530

    backend setup

--- FrontEnd/src/Component/Navbar/Navbar.jsx
+++ FrontEnd/src/Component/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>NavigationTab</div>
+    <>
+        <div className="nav">
+            <img src={assets.logo} className="logo" alt="logo" />
+            
+        </div>
+    </>
   )
 }
 
---
+++ FrontEnd/src/assets/assets.js
@@ -0,0 +1,4 @@
+import logo from './Navbar/APS_LOGO.png';
+export const assets = {
+    logo,
+}
